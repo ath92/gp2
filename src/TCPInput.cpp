823a6dfceacfee87a41d320c6b6843612fac43f8
Tom Hutman <tom@charliebravo.nl>
2015-01-15 11:42:32 +0100

    cout received with ,delimiter

--- src/TCPInput.cpp
+++ src/TCPInput.cpp
@@ -10,5 +10,25 @@ void TCPInput::update(){
 	if(tcp.isConnected()) {
         string str = tcp.receive(); // did anything come in
 		cout << str << endl;
+		
+		std::vector<char> vect;
+
+		std::stringstream ss(str);
+
+		char c;
+
+		while (ss >> c)
+		{
+			vect.push_back(c);
+
+			if (ss.peek() == ','){
+				ss.ignore();
+				//we now have the int in the vector. 
+				string intStr(vect.begin(),vect.end());
+				cout << intStr << endl;
+			}
+		}
+
+		
     }
 }
