ca2be2ab65806574251097193b92c1a2f0846eff
Tom Hutman <tom@charliebravo.nl>
2015-01-22 09:03:07 +0100

    smartxp test 2

--- src/TCPInput.cpp
+++ src/TCPInput.cpp
@@ -65,7 +65,7 @@ void TCPInput::update(){
 			float factoredX = ofGetWidth() - ((*tracker).x+6000) *ofGetWidth()/(float)trackerWidth;
 			float factoredY = ((*tracker).y+40000) *ofGetHeight()/(float)trackerHeight;
 			//invert axes for smartxp setup and do some other things differently
-			factoredX = ((*tracker).y+45000) *ofGetWidth()/(float)50000;
+			factoredX = ((*tracker).y+45000) *ofGetWidth()/(float)53000;
 			factoredY = ((*tracker).x+12000) *ofGetHeight()/(float)68000;
 			p->setPosition(ofVec2f(factoredX , factoredY));//update factors.
 		}
